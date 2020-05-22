94f9a4b13c8d5b4f1b19ee67af2ea06c99daaf4f
Tim Thompson <scubatim84@gmail.com>
2020-05-22 08:55:13 -0600

    Add connection to ingredient DB, conceal DB login credentials

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
+const mongooseConfig = require('./config');
 
 const app = express();
 
@@ -7,9 +9,73 @@ app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 
+let ingredientUserName = process.env.ingredientUserName;
+if (ingredientUserName == undefined || ingredientUserName == null) {
+  ingredientUserName = mongooseConfig.username;
+}
+
+let ingredientPassword = process.env.ingredientPassword;
+if (ingredientPassword == undefined || ingredientPassword == null) {
+  ingredientPassword = mongooseConfig.password;
+}
+
+let ingredientCluster = process.env.ingredientCluster;
+if (ingredientCluster == undefined || ingredientCluster == null) {
+  ingredientCluster = mongooseConfig.cluster;
+}
+
+const mongooseUri = "mongodb+srv://" + ingredientUserName + ":" + ingredientPassword + "@" + ingredientCluster;
+
+mongoose.connect(mongooseUri, {useNewUrlParser: true, useUnifiedTopology: true });
+
+const ingredientSchema = { 
+  name: String 
+};
+
+const Ingredient = mongoose.model("Ingredient", ingredientSchema);
+
 app.route("/")
   .get(function(req, res) {
     res.render("home");
+  });
+
+app.route("/ingredients")
+  .get(function(req, res) {
+    Ingredient.find({}, function(err, foundIngredients) {
+      if (!err) {
+        res.status(200);
+        res.send(foundIngredients);
+      } else {
+        if (!foundIngredients) {
+          res.status(404);
+        } else {
+          res.status(400);
+        }
+      }
+    });
+
+  })
+  
+  .post(function(req, res) {
+    let newIngredient = new Ingredient({
+      name: req.body.name
+    });
+
+    newIngredient.save(function(err) {
+      if(!err) {
+        res.status(201);
+        res.send("Successfully created ingredient.");
+      } else {
+        Ingredient.find({name: newIngredient.name}, function(err, foundIngredient) {
+          if (!foundIngredient) {
+            res.status(200);
+          } else {
+            res.status(204);
+          }
+        });
+      }
+    });
+
   });
 
 let port = process.env.PORT;
@@ -18,6 +84,6 @@ if (port == null || port == "") {
 }
 
 app.listen(port, function() {
-  console.log("Server started running on port 3000.");
+  console.log("Server started running successfully.");
 });
   
