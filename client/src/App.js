355f7bd6eab28dfa47640bd4399d56426d301e7a
Tim Thompson <scubatim84@gmail.com>
2020-06-12 12:21:54 -0600

    Add Login and Register React pages, start Redux implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-			<Landing />
-    </div>
+    <Provider store={store}>
+      <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+      </Router>
+    </Provider>
   );
 }
 
