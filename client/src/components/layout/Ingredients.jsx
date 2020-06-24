3f25d20b35392f0a85488245072fbb280d1923af
Tim Thompson <scubatim84@gmail.com>
2020-06-24 12:13:00 -0600

    Add function to Ingredients component to pull ingredients from API and render in list

--- client/src/components/layout/Ingredients.jsx
+++ client/src/components/layout/Ingredients.jsx
@@ -1,14 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import {Redirect} from 'react-router-dom';
 import {getUserData} from '../../actions/authActions';
+import {getIngredients} from '../../actions/ingredientActions';
+
+import {List, ListItem, ListItemText} from '@material-ui/core';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(2, 2, 2),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(2),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function Ingredients(props) {
+  const classes = useStyles();
+
   const [isLoggedIn, setLoggedIn] = useState(props.isLoggedIn);
   const [user, setUser] = useState({
     email: '',
     firstName: '',
     lastName: '',
   });
+  const [ingredients, setIngredients] = useState({});
+
+  useEffect(async () => {
+    const ingredientData = await getIngredients();
+
+    setIngredients(ingredientData.authResponsePayload);
+  }, []);
 
   useEffect(() => {
     setLoggedIn(props.isLoggedIn);
@@ -31,17 +61,21 @@ function Ingredients(props) {
     }
   }, [isLoggedIn]);
 
-  return !isLoggedIn ? <Redirect to='/login' /> : <div></div>;
+  const createIngredientList = async () => {
+    ingredients.map((ingredient, index) => {
+      return (
+        <ListItem key={index} id={index}>
+          <ListItemText primary={ingredient.name} />
+        </ListItem>
+      );
+    });
+  };
+
+  return !isLoggedIn ? (
+    <Redirect to='/login' />
+  ) : (
+    <List>{createIngredientList}</List>
+  );
 }
 
 export default Ingredients;
-
-/*
-Notes:
-
-Will be a modal
-
-Need form and input text box to type in ingredient name, a list for quantity type (preset values from list like Ounces, Grams, etc.), and then a button to add ingredient and another button to close modal
-
-Form on bottom, on top have a fixed size list that shows current ingredients, which will need to do a GET from API to get list of ingredients in DB, this way user can view before deciding to add or not
-*/
