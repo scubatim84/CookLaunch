383486fa31913752899ff345a3d244879a45c209
Tim Thompson <scubatim84@gmail.com>
2020-07-29 14:25:49 -0600

    Change styling for profile view to be responsive, condense styles for profile and form submit message

--- client/src/Styles.js
+++ client/src/Styles.js
@@ -6,21 +6,6 @@ export const useStylesMain = makeStyles((theme) => ({
   buttonMargin: {
     marginBottom: theme.spacing(1),
   },
-  ingredientMargin: {
-    marginTop: theme.spacing(2),
-  },
-  landingBackground: {
-    backgroundImage: `url(${LandingImage})`,
-    backgroundPosition: '50%',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-  },
-  loginBackground: {
-    backgroundImage: `url(${LoginImage})`,
-    backgroundPosition: '50%',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-  },
   footer: {
     width: '100%',
     position: 'fixed',
@@ -33,9 +18,30 @@ export const useStylesMain = makeStyles((theme) => ({
         ? theme.palette.grey[200]
         : theme.palette.grey[800],
   },
+  formSubmitMessage: {
+    fontSize: '1rem',
+    color: '#ff0000',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  ingredientMargin: {
+    marginTop: theme.spacing(2),
+  },
+  landingBackground: {
+    backgroundImage: `url(${LandingImage})`,
+    backgroundPosition: '50%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  },
   landingPosition: {
     padding: '75px 0px 100px 20px',
     marginLeft: '0px',
+  },
+  loginBackground: {
+    backgroundImage: `url(${LoginImage})`,
+    backgroundPosition: '50%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
   },
   list: {
     width: '100%',
@@ -54,6 +60,14 @@ export const useStylesMain = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
+  profileCard: {
+    marginTop: theme.spacing(1),
+  },
+  profileMargin: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginTop: theme.spacing(2),
+  },
   recipeCard: {
     margin: theme.spacing(2, 2, 2),
   },
@@ -64,6 +78,7 @@ export const useStylesMain = makeStyles((theme) => ({
     margin: theme.spacing(2, 2, 1),
   },
   title: {
+    marginBottom: theme.spacing(1),
     flexGrow: 1,
   },
 }));
@@ -76,33 +91,5 @@ export const useStylesForm = makeStyles((theme) => ({
   card: {
     width: 200,
     height: 100,
-  },
-}));
-
-export const useStylesFormSubmitMsg = makeStyles(() => ({
-  submitMessage: {
-    fontSize: '1rem',
-    color: '#ff0000',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-}));
-
-export const useStylesProfile = makeStyles((theme) => ({
-  root: {
-    margin: theme.spacing(2, 2),
-  },
-  field: {
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-  },
-  title: {
-    paddingTop: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  submit: {
-    paddingBottom: theme.spacing(2),
   },
 }));
--- client/src/components/Profile/ProfileField.jsx
+++ client/src/components/Profile/ProfileField.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import {Grid} from '@material-ui/core';
 import ProfileFieldLabel from './ProfileFieldLabel';
 import ProfileFieldContent from './ProfileFieldContent';
-import {useStylesProfile} from '../../Styles';
-import {themeMain} from '../../Theme';
 
 function ProfileField(props) {
-  const classes = useStylesProfile(themeMain);
-
   return (
     <Grid container>
-      <Grid item xs={12} sm={6} className={classes.field}>
+      <Grid item xs={6} sm={6}>
         <ProfileFieldLabel label={props.label} />
       </Grid>
-      <Grid item xs={12} sm={6} className={classes.field}>
+      <Grid item xs={6} sm={6}>
         <ProfileFieldContent
           handleChange={props.handleChange}
           label={props.label}
