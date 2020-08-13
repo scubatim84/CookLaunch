5b5cadb46d22aa746d4dbc3a9f9e2fa5401f7c2a
Tim Thompson <scubatim84@gmail.com>
2020-08-12 20:30:32 -0600

    Add delete route to user API

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 const isEmpty = require('is-empty');
 
+// Load User model
+const User = require('../../models/User');
+
 // @route GET api/user/profile
 // @desc Obtain user profile
 // @access Private
@@ -55,6 +58,33 @@ router.put('/profile', async (req, res) => {
     }
   } else {
     return res.status(404).json('User not found');
+  }
+});
+
+// @route DELETE api/user/:id
+// @desc Delete user by ID
+// @access Private
+router.delete('/:id', async (req, res) => {
+  // Obtain user from request to verify token
+  const foundUser = req.user;
+
+  // Once user is verified, delete user
+  if (foundUser) {
+    try {
+      const deletedUser = await User.deleteOne({
+        _id: req.params.id,
+      });
+
+      if (deletedUser.deletedCount === 1) {
+        res.status(204).json(null);
+      } else {
+        res.status(400).json('User deletion failed.');
+      }
+    } catch (err) {
+      res.status(400).json('An error has occurred. ' + err);
+    }
+  } else {
+    res.status(400).json('No user found in database.');
   }
 });
 
