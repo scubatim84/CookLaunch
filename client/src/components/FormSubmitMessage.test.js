7f44b3c65c82602940f39647434d39eb32861322
Tim Thompson <scubatim84@gmail.com>
2020-09-04 18:19:32 -0600

    Add coverage to FormSubmitMessage unit test for passing submit message prop to component

--- client/src/components/FormSubmitMessage.test.js
+++ client/src/components/FormSubmitMessage.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import FormSubmitMessage from './FormSubmitMessage';
+import Typography from '@material-ui/core/Typography';
+
+const wrapper = shallow(<FormSubmitMessage submitMessage='Test message' />);
 
 it('Renders component without crashing', () => {
   shallow(<FormSubmitMessage />);
+});
+
+it('Renders submit message passed as prop to component', () => {
+  expect(wrapper.equals(<Typography>Test message</Typography>));
 });
