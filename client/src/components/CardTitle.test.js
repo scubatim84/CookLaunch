89e0e9060a39fd2d51b3b02bf8afd9fdbc621976
Tim Thompson <scubatim84@gmail.com>
2020-09-04 18:14:08 -0600

    Add coverage to CardTitle unit test for passing title prop to component

--- client/src/components/CardTitle.test.js
+++ client/src/components/CardTitle.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import CardTitle from './CardTitle';
+import Typography from '@material-ui/core/Typography';
+
+const wrapper = shallow(<CardTitle title='Test Title' />);
 
 it('Renders component without crashing', () => {
-  shallow(<CardTitle />);
+  shallow(<CardTitle title='Test Title' />);
+});
+
+it('Renders title passed as prop to component', () => {
+  expect(
+    wrapper.equals(
+      <Typography component='h1' variant='h5'>
+        Test Title
+      </Typography>
+    )
+  );
 });
