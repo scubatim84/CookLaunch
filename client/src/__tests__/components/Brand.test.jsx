14e82074c6efbeec706e486401916e9f554c8375
Tim Thompson <scubatim84@gmail.com>
2020-09-15 08:12:41 -0600

    Relabel Brand component which got missed in test relabeling, add package-lock.json to git

--- client/src/__tests__/components/Brand.test.jsx
+++ client/src/__tests__/components/Brand.test.jsx
@@ -3,7 +3,9 @@ import {shallow} from 'enzyme';
 import Brand from '../../components/Brand';
 
 describe('Brand', () => {
-  it('Renders component without crashing', () => {
-    shallow(<Brand />);
+  it('Renders correctly', () => {
+    const wrapper = shallow(<Brand />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
