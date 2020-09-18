f045c05dfbe20db5570aff256f858a98a1354b60
Tim Thompson <scubatim84@gmail.com>
2020-09-18 06:46:09 -0600

    Fix brand unit test to remove snapshot code

--- client/src/__tests__/components/Brand.test.jsx
+++ client/src/__tests__/components/Brand.test.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Brand from '../../components/Brand';
 
 describe('Brand', () => {
   it('Renders correctly', () => {
-    const wrapper = shallow(<Brand />);
-
-    expect(wrapper).toMatchSnapshot();
+    shallow(<Brand />);
   });
 });
