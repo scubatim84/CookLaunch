d469449afaa0d698684611ada55cd73ccc77fa5b
Tim Thompson <scubatim84@gmail.com>
2020-09-20 07:10:13 -0600

    Add integration tests for both api/auth/register and api/auth/login API routes

--- __tests__/routes/api/auth.test.js
+++ __tests__/routes/api/auth.test.js
@@ -1,8 +1,63 @@
 import request from 'supertest';
+import mongoose from 'mongoose';
 import app from '../../../server.js';
 
+describe('POST /api/auth/register', () => {
+  test('Responds with status code 400 and error if new user data not valid', async () => {
+    const newUserMock = {
+      firstName: 'test',
+      lastName: 'runner',
+      email: '',
+      password: 'tester',
+      password2: 'tester',
+    };
+
+    let res = await request(app).post('/api/auth/register').send(newUserMock);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toBe('Email field is required');
+  });
+
+  test('Responds with status code 201 and JSON of new user data if success', async () => {
+    const newUserMock = {
+      firstName: 'test',
+      lastName: 'runner',
+      email: 'test@runner.com',
+      password: 'tester',
+      password2: 'tester',
+    };
+
+    let res = await request(app).post('/api/auth/register').send(newUserMock);
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.email).toBe(newUserMock.email);
+    expect(res.body.firstName).toBe(newUserMock.firstName);
+    expect(res.body.lastName).toBe(newUserMock.lastName);
+    expect(res.body.pantry).toBeDefined();
+    expect(res.body.groceries).toBeDefined();
+    expect(res.body._id).not.toBeNull();
+    expect(res.body.date).not.toBeNull();
+    expect(res.body.password).not.toBe(newUserMock.password);
+  });
+
+  test('Responds with status code 400 and error message if user already exists', async () => {
+    const newUserMock = {
+      firstName: 'test',
+      lastName: 'runner',
+      email: 'test@runner.com',
+      password: 'tester',
+      password2: 'tester',
+    };
+
+    let res = await request(app).post('/api/auth/register').send(newUserMock);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toBe('Email already exists');
+  });
+}, 30000);
+
 describe('POST /api/auth/login', () => {
-  test('Responds with token if successful, status code 400 if not', async () => {
+  test('Responds with status code 400 and error if existing user data not valid', async () => {
     const existingUserMock = {
       email: 'testrunner@runner.com',
       password: '',
@@ -11,10 +66,60 @@ describe('POST /api/auth/login', () => {
     let res = await request(app).post('/api/auth/login').send(existingUserMock);
 
     expect(res.statusCode).toBe(400);
+    expect(res.body).toBe('Password field is required');
+  });
 
-    existingUserMock.password = 'tester';
+  test('Responds with status code 404 and error if user not found', async () => {
+    const existingUserMock = {
+      email: 'testrunner@dontexist.com',
+      password: 'tester',
+    };
 
-    res = await request(app).post('/api/auth/login').send(existingUserMock);
+    let res = await request(app).post('/api/auth/login').send(existingUserMock);
+
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toBe('Email not found');
+  });
+
+  test('Responds with status code 400 and error if password incorrect', async () => {
+    const newUserMock = {
+      firstName: 'test',
+      lastName: 'runner',
+      email: 'test@tester.com',
+      password: 'tester',
+      password2: 'tester',
+    };
+
+    await request(app).post('/api/auth/register').send(newUserMock);
+
+    const existingUserMock = {
+      email: 'test@tester.com',
+      password: 'badpassword',
+    };
+
+    let res = await request(app).post('/api/auth/login').send(existingUserMock);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toBe('Password incorrect');
+  });
+
+  test('Responds with status code 200 and token if successful', async () => {
+    const newUserMock = {
+      firstName: 'test',
+      lastName: 'runner',
+      email: 'tester@tester.com',
+      password: 'tester',
+      password2: 'tester',
+    };
+
+    await request(app).post('/api/auth/register').send(newUserMock);
+
+    const existingUserMock = {
+      email: 'tester@tester.com',
+      password: 'tester',
+    };
+
+    let res = await request(app).post('/api/auth/login').send(existingUserMock);
 
     expect(res.statusCode).toBe(200);
     expect(res.body).toContain('Bearer');
