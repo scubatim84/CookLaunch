68cba668c2e51f5ffd5cbeb8d6d0526841a551ce
Tim Thompson <scubatim84@gmail.com>
2020-10-10 16:03:05 -0600

    Add unit tests to LoginForm test component to test user input and test IDs in LoginForm component, add unit test to Profile test component to test router redirect

--- client/src/__tests__/components/Profile/Profile.test.jsx
+++ client/src/__tests__/components/Profile/Profile.test.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import UserEvent from '@testing-library/user-event';
+import { shallow } from 'enzyme';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Profile from '../../../components/Profile/Profile';
+import Loader from '../../../components/Loader';
 
 describe('Profile', () => {
   it('Renders component without crashing', () => {
     shallow(<Profile />);
+  });
+
+  it('Redirects user to login component if not logged in', () => {
+    render(
+      <Router>
+        <Profile />
+      </Router>
+    );
   });
 });
