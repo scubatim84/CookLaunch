998196bd5553e01e80e58763b3cee146a0a2d629
Tim Thompson <scubatim84@gmail.com>
2020-10-21 08:31:27 -0600

    Add missing id check logic to getOneRecipe function, add unit tests for getOneRecipe function, add unit tests for deleteRecipe function

--- client/src/__tests__/actions/recipeActions.test.js
+++ client/src/__tests__/actions/recipeActions.test.js
@@ -1,7 +1,12 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
-import { addRecipe, getAllRecipes } from '../../actions/recipeActions';
+import {
+  addRecipe,
+  deleteRecipe,
+  getAllRecipes,
+  getOneRecipe,
+} from '../../actions/recipeActions';
 
 const server = setupServer();
 
@@ -33,7 +38,6 @@ describe('getAllRecipes function', () => {
 
     server.use(
       rest.get('/api/recipes/', (req, res, ctx) => {
-        // Respond with "500 Internal Server Error" status for this test.
         return res(ctx.status(500), ctx.json(errorMessage));
       })
     );
@@ -95,6 +99,82 @@ describe('addRecipe function', () => {
     );
 
     const response = await addRecipe(testRecipe);
+
+    expect(response).toEqual(errorMessage);
+  });
+});
+
+describe('getOneRecipe function', () => {
+  const recipeId = 'testid';
+
+  it('Tests function when id is empty', async () => {
+    const emptyId = '';
+    const response = await getOneRecipe(emptyId);
+
+    expect(response.data).toEqual('An error has occurred. Please try again.');
+  });
+
+  it('Tests successful API get request to obtain one recipe owned by user', async () => {
+    server.use(
+      rest.get(`/api/recipes/${recipeId}`, (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json('test'));
+      })
+    );
+
+    const response = await getOneRecipe(recipeId);
+
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual('test');
+  });
+
+  it('Tests failed API get request to obtain one recipe owned by user', async () => {
+    const errorMessage = 'An error message';
+
+    server.use(
+      rest.get(`/api/recipes/${recipeId}`, (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json(errorMessage));
+      })
+    );
+
+    const response = await getOneRecipe(recipeId);
+
+    expect(response.response.data).toEqual(errorMessage);
+  });
+});
+
+describe('deleteRecipe function', () => {
+  const recipeId = 'testid';
+
+  it('Tests function when id is empty', async () => {
+    const emptyId = '';
+    const response = await deleteRecipe(emptyId);
+
+    expect(response.data).toEqual('An error has occurred. Please try again.');
+  });
+
+  it('Tests successful API get request to delete one recipe owned by user', async () => {
+    server.use(
+      rest.delete(`/api/recipes/${recipeId}`, (req, res, ctx) => {
+        return res(ctx.status(204), ctx.json(null));
+      })
+    );
+
+    const response = await deleteRecipe(recipeId);
+
+    expect(response.status).toBe(204);
+    expect(response.data).toEqual(null);
+  });
+
+  it('Tests failed API get request to delete one recipe owned by user', async () => {
+    const errorMessage = 'An error message';
+
+    server.use(
+      rest.delete(`/api/recipes/${recipeId}`, (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json(errorMessage));
+      })
+    );
+
+    const response = await deleteRecipe(recipeId);
 
     expect(response).toEqual(errorMessage);
   });
