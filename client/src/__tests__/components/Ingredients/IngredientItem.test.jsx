9b92f25bcb181afa70c968b74013aaeeff1bcc36
Tim Thompson <scubatim84@gmail.com>
2020-10-24 21:18:08 -0600

    Add more unit tests for IngredientItem.test.jsx

--- client/src/__tests__/components/Ingredients/IngredientItem.test.jsx
+++ client/src/__tests__/components/Ingredients/IngredientItem.test.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import UserEvent from '@testing-library/user-event';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { shallow } from 'enzyme';
 import IngredientItem from '../../../components/Ingredients/IngredientItem';
 
 describe('IngredientItem', () => {
+  const testIngredient = {
+    id: 'testid',
+    name: 'ingredientname',
+    quantity: '3',
+    quantityType: 'Ounces',
+    checked: false,
+    groceryExtra: false,
+  };
+
+  const handleDelete = jest.fn((id) => id);
+
   it('Renders component without crashing', () => {
     shallow(<IngredientItem />);
   });
 
   it('Renders ingredient item when unchecked and not a grocery ingredient', () => {
-    const testIngredient = {
-      id: 'testid',
-      name: 'ingredientname',
-      quantity: '3',
-      quantityType: 'Ounces',
-      checked: false,
-      groceryExtra: false,
-    };
-
     const { queryByTestId } = render(
       <IngredientItem
         id={testIngredient.id}
@@ -27,6 +29,7 @@ describe('IngredientItem', () => {
         quantityType={testIngredient.quantityType}
         checked={testIngredient.checked}
         groceryExtra={testIngredient.groceryExtra}
+        handleDelete={handleDelete}
       />
     );
 
@@ -43,8 +46,29 @@ describe('IngredientItem', () => {
     expect(queryByTestId('edit-icon')).toBeTruthy();
     expect(queryByTestId('delete-icon')).toBeTruthy();
     expect(queryByTestId('delete-dialog')).toBeNull();
+  });
+
+  it('IngredientDeleteDialog component functions correctly', async () => {
+    const { queryByTestId } = render(
+      <IngredientItem
+        id={testIngredient.id}
+        name={testIngredient.name}
+        quantity={testIngredient.quantity}
+        quantityType={testIngredient.quantityType}
+        checked={testIngredient.checked}
+        groceryExtra={testIngredient.groceryExtra}
+        handleDelete={handleDelete}
+      />
+    );
 
     UserEvent.click(queryByTestId('delete-icon'));
     expect(queryByTestId('delete-dialog')).toBeTruthy();
+
+    UserEvent.click(queryByTestId('delete-dialog-cancel'));
+    await waitFor(() => expect(queryByTestId('delete-dialog')).toBeNull());
+
+    UserEvent.click(queryByTestId('delete-icon'));
+    UserEvent.click(queryByTestId('delete-dialog-delete'));
+    await waitFor(() => expect(handleDelete).toHaveBeenCalledTimes(1));
   });
 });
