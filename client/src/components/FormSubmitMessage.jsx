f6b8e26035d1384050d95700ff1a0e07832fa60a
Tim Thompson <scubatim84@gmail.com>
2020-10-25 18:49:52 -0600

    Add unit test for editing ingredient field, add unit test for rendering error during ingredient edit

--- client/src/components/FormSubmitMessage.jsx
+++ client/src/components/FormSubmitMessage.jsx
@@ -6,7 +6,11 @@ const FormSubmitMessage = (props) => {
   const classes = useStyles();
 
   return (
-    <Typography id='Message' className={classes.formSubmitMessage}>
+    <Typography
+      data-testid={'form-submit-message'}
+      id='Message'
+      className={classes.formSubmitMessage}
+    >
       {props.submitMessage}
     </Typography>
   );
