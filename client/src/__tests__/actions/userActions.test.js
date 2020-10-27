cbaa7343883f5e8483632179eb9072956872381e
Tim Thompson <scubatim84@gmail.com>
2020-10-27 06:55:46 -0600

    Remove unnecessary code in userActions.js, add unit test for updateUserProfile

--- client/src/__tests__/actions/userActions.test.js
+++ client/src/__tests__/actions/userActions.test.js
@@ -1,7 +1,8 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import { REQUEST_SUCCESS, REQUEST_FAIL } from '../../actions/types';
 
-import { getUserData } from '../../actions/userActions';
+import { getUserData, updateUserProfile } from '../../actions/userActions';
 
 const server = setupServer();
 
@@ -40,5 +41,70 @@ describe('getUserData function', () => {
     const response = await getUserData();
 
     expect(response).toEqual(errorMessage);
+  });
+});
+
+describe('updateUserProfile function', () => {
+  const userData = {
+    firstName: '',
+    lastName: 'runner',
+    email: 'test@runner.com',
+  };
+
+  it('Empty first name returns error', async () => {
+    const response = await updateUserProfile(userData);
+
+    expect(response.authResponseType).toBe(REQUEST_FAIL);
+    expect(response.authResponsePayload).toEqual('Please enter a first name.');
+  });
+
+  it('Empty last name returns error', async () => {
+    userData.firstName = 'test';
+    userData.lastName = '';
+
+    const response = await updateUserProfile(userData);
+
+    expect(response.authResponseType).toBe(REQUEST_FAIL);
+    expect(response.authResponsePayload).toEqual('Please enter a last name.');
+  });
+
+  it('Empty email returns error', async () => {
+    userData.lastName = 'runner';
+    userData.email = '';
+
+    const response = await updateUserProfile(userData);
+
+    expect(response.authResponseType).toBe(REQUEST_FAIL);
+    expect(response.authResponsePayload).toEqual('Please enter an email.');
+  });
+
+  it('Tests successful API put request to update user profile', async () => {
+    userData.email = 'test@runner.com';
+
+    server.use(
+      rest.put('/api/user/profile', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(userData));
+      })
+    );
+
+    const response = await updateUserProfile(userData);
+
+    expect(response.authResponseType).toBe(REQUEST_SUCCESS);
+    expect(response.authResponsePayload).toEqual(userData);
+  });
+
+  it('Tests failed API put request to update user profile', async () => {
+    const errorMessage = 'An error message';
+
+    server.use(
+      rest.put('/api/user/profile', (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json(errorMessage));
+      })
+    );
+
+    const response = await updateUserProfile(userData);
+
+    expect(response.authResponseType).toBe(REQUEST_FAIL);
+    expect(response.authResponsePayload).toEqual(errorMessage);
   });
 });
