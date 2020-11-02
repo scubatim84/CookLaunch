6cc55bd1129ce8008ccbb62c229ae6508e4a0e45
Tim Thompson <scubatim84@gmail.com>
2020-11-01 21:43:54 -0700

    Add test data and exclude it from jest coverage & watch, add unit tests to cover all possible renders of IngredientNames component, refactor IngredientNames render to reduce nesting

---
+++ client/src/__tests__/testData.js
@@ -0,0 +1,29 @@
+export const ingredientData = [
+  {
+    _id: 'testIngredientId1',
+    dateLastChanged: new Date(),
+    checked: false,
+    groceryExtra: false,
+    name: 'INGREDIENT ONE',
+    createdBy: 'testUser',
+    __v: 0,
+  },
+  {
+    _id: 'testIngredientId2',
+    dateLastChanged: new Date(),
+    checked: false,
+    groceryExtra: false,
+    name: 'INGREDIENT TWO',
+    createdBy: 'testUser',
+    __v: 0,
+  },
+  {
+    _id: 'testIngredientId3',
+    dateLastChanged: new Date(),
+    checked: false,
+    groceryExtra: false,
+    name: 'INGREDIENT THREE',
+    createdBy: 'testUser',
+    __v: 0,
+  },
+];
